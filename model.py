938a4a8052a112991700ef374330d2f2d850b230
geeek <hristoforov.ilya@gmail.com>
2020-05-10 18:48:19 +0300

    model changes

--- model.py
+++ model.py
@@ -1,67 +1,34 @@
+import numpy as np
 import pandas as pd
 import warnings
+from sklearn.metrics import roc_auc_score
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.model_selection import StratifiedKFold
 
 warnings.filterwarnings('ignore')
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import roc_auc_score
-from sklearn.linear_model import LogisticRegression
-
 df = pd.read_csv('data/prepared_data.csv', sep=';')
 
-# разделим признаки на числовые и категориальные
-num_cols = ['Course_averageScore',
-            'Course_passingScore',
-            'Test_Qnt',
-            'Test_durationAvg',
-            'Student_MessagesQnt']
-
-obj_cols = ['Course_visibilityStatus',
-            'Course_periodicity']
-
-# Кодируем категориальные признаки
-ohe_df = pd.DataFrame(index=df['student_id'])
-ohe = OneHotEncoder(handle_unknown='ignore')
-
-for col in obj_cols:
-    ohe.fit(df[[col]])
-
-    ohe_result = pd.DataFrame(ohe.transform(df[[col]]).toarray(),
-                              columns=ohe.get_feature_names(input_features=[col]),
-                              index=df['student_id'])
-    ohe_df = ohe_df.join(ohe_result)
-
-# Стандартизируем числовые признаки
-std_df = pd.DataFrame(index=df['student_id'])
-scaler = StandardScaler()
-
-for col in num_cols:
-    scaler.fit(df[[col]])
-    std_result = pd.DataFrame(scaler.transform(df[[col]]),
-                              columns=[col],
-                              index=df['student_id'])
-    std_df = std_df.join(std_result, on='student_id', how='left', lsuffix='_left', rsuffix='_right')
-
-    X = ohe_df.join(std_df)
 y = df['Churn']
+X = df.drop(['Churn'], axis=1)
 
-# Отделяем тестовую выборку
-X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.3, stratify=y, random_state=18)
+# Обучаем логистическую регрессию на Kfold вместо train_test_split
+# Auto selecting the best Regularization value (Cs)
+skf = StratifiedKFold(n_splits=4, shuffle=True, random_state=33)
+c_values = np.logspace(-2, 3, 100)
+lr = LogisticRegressionCV(Cs=c_values, cv=skf, verbose=0, n_jobs=-1, scoring="roc_auc", max_iter=1000)
+lr.fit(X.values, y.values)
 
-# Обучаем логистическую регрессию
-lr = LogisticRegression(C=1.0, random_state=18, n_jobs=-1).fit(X_train, y_train)
-
-# Делаем прогноз и оцениваем результат
-y_pred_test = lr.predict_proba(X_test)[:, 1]
-score = roc_auc_score(y_test, y_pred_test)
+# The best regularization parameter Cs (L2) and ROC AUC Score
+Cs = lr.C_
+score = lr.score(X, y)
+print('The best L2: ', Cs, '\n\r', 'ROC AUC: ', score)
 
 # Для примера предсказываем значения для всей выборки
 y_pred = lr.predict_proba(X)[:, 1]
-results = list(zip(df['student_id'], y_pred, y))
-results_df = pd.DataFrame(results, columns=['student_id', 'prob_predicted', 'Real_value'])
+results = list(zip(df['Student_id'], y_pred, y))
+results_df = pd.DataFrame(results, columns=['Student_id', 'prob_predicted', 'Real_value'])
 results_df['Predicted_value'] = results_df['prob_predicted'].apply(lambda x: 1 if x >= 0.81 else 0)
 
 # Сохраняем результат
-df.to_csv('data/churn_prediction.csv', index=False, sep=';')
+results_df.to_csv('data/churn_prediction.csv', index=False, sep=';')
